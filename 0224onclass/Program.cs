7a07424410b28f5035643e2675927855b90c92c3
yue <xiaobaiwhu@gmail.com>
2020-02-24 09:04:20 +0800

    0224 class

---
+++ 0224onclass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _0224onclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入数据");
+            int j = 2;
+            int i = Convert.ToInt32(Console.ReadLine());
+            while (i > 1)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        Console.WriteLine(j);
+                        break;
+                    }
+                }
+                i = i / j;
+            }
+            Console.WriteLine(j);
+
+        }
+    }
+}
---
+++ onclass2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace onclass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数组数据为：");
+            int[] a = new int[] { 2, 4, 6, 7, 9, 17, 23, 25, 26 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(" " + a[i]);
+            }
+            Console.WriteLine();
+            int max = a[0];
+            for(int i = 1; i < a.Length; i++)
+            {
+                if (max < a[i])
+                    max = a[i];
+            }
+            int min = a[0];
+            for(int i = 1; i < a.Length; i++)
+            {
+                if (min > a[i])
+                    min = a[i];
+            }
+            int sum=0;
+            for(int i=1;i<a.Length;i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("max="+max);
+            Console.WriteLine("min=" + min);
+            Console.WriteLine("sum=" +sum);
+
+        }
+    }
+}
